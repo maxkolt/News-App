d0159727e50796f1378c352ea698eb7844065649
maxkolt <12345kolt@gmail.com>
2021-02-21 16:24:38 +0300

    Проект NewsApp

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Custom Http Module
 function customHttp() {
     return {
         get(url, cb) {
@@ -52,61 +53,139 @@ function customHttp() {
         },
     };
 }
-
 // Init http module
 const http = customHttp();
 
-const newsService = (function () {
-    apiKey = '8dd71c05c0724b9cb72c982ff64d9925';
-    apiUrl = 'https://news-api-v2.herokuapp.com';
+const newsService = (function() {
+    const apiKey = '8dd71c05c0724b9cb72c982ff64d9925'
+    const apiUrl = 'https://news-api-v2.herokuapp.com';
 
     return {
         topHeadlines(country = 'ua', cb) {
-            http.get(`${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`)
+            http.get(
+                `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+                cb,
+            );
         },
         everything(query, cb) {
-            http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb)
-        }
-    }
+            http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+        },
+    };
 })();
 
+// Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    loadNews();
+});
+
 //  init selects
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
     loadNews();
 });
 
+// Load news function
 function loadNews() {
-    newsService.topHeadlines('ua', onGetResponse)
+    showLoader();
+
+    const country = countrySelect.value;
+    const searchText = searchInput.value;
+
+    if (!searchText) {
+        newsService.topHeadlines(country, onGetResponse);
+    } else {
+        newsService.everything(searchText, onGetResponse);
+    }
 }
 
-
+// Function on get response from server
 function onGetResponse(err, res) {
+    removePreloader();
+
+    if (err) {
+        showAlert(err, 'error-msg');
+        return;
+    }
+
+    if (!res.articles.length) {
+        // show empty message
+        return;
+    }
+
     renderNews(res.articles);
 }
 
-
+// Function render news
 function renderNews(news) {
     const newsContainer = document.querySelector('.news-container .row');
+    if (newsContainer.children.length) {
+        clearContainer(newsContainer);
+    }
+    let fragment = '';
 
     news.forEach(newsItem => {
-        const el = newsTomplate(newsItem)
-    })
+        const el = newsTemplate(newsItem);
+        fragment += el;
+    });
+
+    newsContainer.insertAdjacentHTML('afterbegin', fragment);
 }
 
+// Function clear container
+function clearContainer(container) {
+    // container.innerHTML = '';
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
 
-function newsTomplate({urlToImage, title, url, description}) {
-console.log(news);
-return`
-<div class = "col s12">
-<div class="card">
-<div class="card-image">
-    <img src="${urlToImage}">
-    <span class="card-title">${title || ''}</span>
-</div>
-<div class="card-content"></div>
-</div>
+// News item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+    return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage}">
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
 
-</div>
-`;
+function showAlert(msg, type = 'success') {
+    M.toast({ html: msg, classes: type });
+}
+
+//  Show loader function
+function showLoader() {
+    document.body.insertAdjacentHTML(
+        'afterbegin',
+        `
+    <div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+  `,
+    );
+}
+
+// Remove loader function
+function removePreloader() {
+    const loader = document.querySelector('.progress');
+    if (loader) {
+        loader.remove();
+    }
 }
